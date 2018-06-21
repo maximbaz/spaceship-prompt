d1302d8cf5dfed94e94510321e32fe3e2e1959ca
Cyril Levis <levis.cyril@gmail.com>
2018-06-21 17:43:44 +0200

    thanks for advises change icon run if tox.ini in the project

--- sections/tox.zsh
+++ sections/tox.zsh
@@ -7,15 +7,18 @@
 # ------------------------------------------------------------------------------
 # Configuration
 # ------------------------------------------------------------------------------
-
 SPACESHIP_TOX_SHOW="${SPACESHIP_TOX_SHOW=true}"
 SPACESHIP_TOX_PREFIX="${SPACESHIP_TOX_PREFIX=""}"
 SPACESHIP_TOX_SUFFIX="${SPACESHIP_TOX_SUFFIX="$SPACESHIP_PROMPT_DEFAULT_SUFFIX"}"
-SPACESHIP_TOX_SYMBOL="${SPACESHIP_TOX_SYMBOL="  "}"
+SPACESHIP_TOX_COLOR_FAIL="${SPACESHIP_TOX_COLOR_FAIL="red"}"
+SPACESHIP_TOX_COLOR_OK="${SPACESHIP_TOX_COLOR_OK="green"}"
+SPACESHIP_TOX_FAIL_ICON="${SPACESHIP_TOX_FAIL_ICON="\u2718 "}"
+SPACESHIP_TOX_OK_ICON="${SPACESHIP_TOX_OK_ICON="\u2714 "}"
 
 # ------------------------------------------------------------------------------
 # Section
 # ------------------------------------------------------------------------------
+
 
 spaceship_async_job_load_tox() {
   [[ $SPACESHIP_TOX_SHOW == false ]] && return
@@ -23,32 +26,23 @@ spaceship_async_job_load_tox() {
   async_job spaceship spaceship_async_job_tox "$PWD"
 }
 
-tox_test() {
-  # test code if tox config present
-  { command tox -q >/dev/null 2>&1 \
-    && { ret_code='OK' } \
-    || {  ret_code='FAIL' }}
-  echo $ret_code
-}
-
 spaceship_async_job_tox() {
   builtin cd -q "$1" 2>/dev/null
-  # test directly if tox.ini present in current folder...
-  { [[ -f tox.ini ]] && tox_test } || \
-    # ...or try tox return, little longer
-    { command tox --notest 2>&1 | grep -q 'ERROR: toxini' && return "" || tox_test }
+  setopt extendedglob
+  [[ -f $(echo (../)#tox.ini(:a)) ]] || return
+  tox -q >/dev/null 2>&1 && echo 'OK' || echo 'FAIL'
 }
 
 # Shows tox status
 spaceship_tox() {
   [[ -z "${SPACESHIP_ASYNC_RESULTS[spaceship_async_job_tox]}" ]] && return
-  [[ "${SPACESHIP_ASYNC_RESULTS[spaceship_async_job_tox]}" == "OK" ]] && SPACESHIP_TOX_COLOR="green"
-  [[ "${SPACESHIP_ASYNC_RESULTS[spaceship_async_job_tox]}" == "FAIL" ]] && SPACESHIP_TOX_COLOR="red"
+  [[ "${SPACESHIP_ASYNC_RESULTS[spaceship_async_job_tox]}" == "OK" ]] && { SPACESHIP_TOX_COLOR="${SPACESHIP_TOX_COLOR_OK}"; SPACESHIP_TOX_SYMBOL="${SPACESHIP_TOX_OK_ICON}" }
+  [[ "${SPACESHIP_ASYNC_RESULTS[spaceship_async_job_tox]}" == "FAIL" ]] && { SPACESHIP_TOX_COLOR="${SPACESHIP_TOX_COLOR_FAIL}"; SPACESHIP_TOX_SYMBOL="${SPACESHIP_TOX_FAIL_ICON}" }
 
   [[ $SPACE ]]
   spaceship::section \
     "$SPACESHIP_TOX_COLOR" \
     "$SPACESHIP_TOX_PREFIX" \
-    "${SPACESHIP_TOX_SYMBOL}${SPACESHIP_ASYNC_RESULTS[spaceship_async_job_tox]}" \
+    "$SPACESHIP_TOX_SYMBOL" \
     "$SPACESHIP_TOX_SUFFIX"
 }
